9fe72f9947f6bd17bc44c0301be14f72c4614c04
Richard Davy <33124789+RichardDavy84@users.noreply.github.com>
2024-09-01 18:54:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    } else {
+        mobileMenu.style.display = 'block';
+    }
+}
+
